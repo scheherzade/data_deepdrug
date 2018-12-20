77e336a0613de87c1e6bd87758bd2b9bce679e2b
Shahrzad Shirzad <sshirz1@shelob1.(none)>
2018-12-20 17:19:19 -0600

    Cleaning up the files

--- scripts/RunCNN.sh
+++ scripts/RunCNN.sh
@@ -2,9 +2,10 @@
 python_script='CNN_11_13.py'
 date_str='2018-11-13-1221'
 curr_date=`date '+%Y-%m-%d-%H%M'`
-results_dir="/home/sshirzad/workspace/deepdrug/results/$date_str/$curr_date"
+dir=
+results_dir=$dir/$date_str/$curr_date"
 mkdir -p ${results_dir}
 cd ${results_dir}
-ipython /home/sshirzad/workspace/deepdrug/python_scripts/${python_script} | tee ${results_dir}_1
-cp /home/sshirzad/workspace/deepdrug/python_scripts/${python_script} ${results_dir}
+ipython $dir/python_scripts/${python_script} | tee ${results_dir}_1
+cp $dir/python_scripts/${python_script} ${results_dir}
 
